a4d4f0f9d837f1d4823ee177ca342f597d57eb07
THIS IS NOT ME <92795413+stormappears@users.noreply.github.com>
2023-10-30 19:27:51 +0530

    server udate

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require("express")();
 var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+var io = require("socket.io")(http, {
+  transports: ["websocket"],
+});
 
 app.get("/view", (req, res) => {
   res.sendFile(__dirname + "/display.html");
@@ -8,11 +10,13 @@ app.get("/view", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("join-message", (roomId) => {
+    socket.sendBuffer = [];
     socket.join(roomId);
     console.log("User joined in a room : " + roomId);
   });
 
   socket.on("screen-data", function (data) {
+    socket.sendBuffer = [];
     data = JSON.parse(data);
     var room = data.room;
     var imgStr = data.image;
